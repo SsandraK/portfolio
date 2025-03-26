4004f52bf4777fcde4098d31d6ba72e2e3aff9ef
Sandra Solovjov <sandrasolovjov@Sandras-MacBook-Pro-2.local>
2025-03-26 11:25:26 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import github from "@astrojs/github";
 
 export default defineConfig({
-  site: 'https://sandrakannik.github.io', 
-  base: '/', 
-  output: "static",
-  adapter: github(),  
   integrations: [tailwind()],
 });
